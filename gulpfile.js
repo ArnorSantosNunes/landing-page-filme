fc047603f711bef8e6b28ba8236519404eb311b1
arnor <arnorsantosnunes01@gmail.com>
2025-02-19 21:34:00 -0300

    iniciando projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,37 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 function styles(){
 
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputstyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+    return gulp.src([
+        './node_modules/bootstrap/scss/bootstrap.scss', // Bootstrap SCSS
+        './src/styles/*.scss' // Seus estilos personalizados
+    ])
+        .pipe(sass({ quietDeps: true }).on('error', sass.logError))
+        .pipe(concat('styles.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/css'))
 }
-exports.default = styles;
+function scripts() {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', // Bootstrap JS
+        './src/js/main.js' // Seus scripts personalizados
+    ])
+        .pipe(concat('scripts.js')) // Concatena os arquivos em um único arquivo
+        .pipe(gulp.dest('./dist/js')); // Salva o arquivo na pasta de destino
+}
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function (){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/js/*.js', gulp.parallel(scripts));
 }
